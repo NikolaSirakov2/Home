4d8e44bb0592de2eba0e78a27d9297ddc1250971
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-01-01 18:39:42 +0200

    Update 17.js

--- 17.js
+++ 17.js
@@ -1,1 +1,13 @@
-let arr1 = [];
+let arr1 =  [1, 3, 2, 4, 3, 2];
+let ZigZag = false;
+
+for(let i = 1; i < arr1.length; i++){
+    let decend = arr1[i] < arr1[i-1] && arr1[i] < arr1[i+1];
+    let acend = arr1[i] > arr1[i-1] && arr1[i] > arr1[i+1];
+
+    if(decend || acend){
+        ZigZag = true;
+    }
+}
+
+console.log(ZigZag);
